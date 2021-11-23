ef0b4964e3ba7b19119ffc8d8d3260e272b8deb3
baldbros <82809748+baldbros@users.noreply.github.com>
2021-11-23 15:57:33 +0100

    Create Python-RPG.py
    Fundamentals of Python-RPG

---
+++ Python-RPG.py
@@ -0,0 +1,14 @@
+hero_base_level = 1
+hero_base_strength = 5
+hero_base_dexterity = 5
+hero_level = input()
+def enemy():
+    print('   @ \n / | \ \n  / \ ')
+
+def hero():
+    print(' _\|/^ \n (_oo \n   | \n / | \ \n   | \n   LL')
+
+def hero_stats():
+    if hero_level == 1:
+        hero_dexterity == hero_base
+
