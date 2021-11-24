3ed170f88dee1dfdf873f356930786098668d315
baldbros <82809748+baldbros@users.noreply.github.com>
2021-11-24 13:02:03 +0100

    Update Python-RPG.py
    Added Monster class

--- Python-RPG.py
+++ Python-RPG.py
@@ -2,15 +2,39 @@ import json
 
 data = json.load(open("data.txt"))
 
+#Character experience gain part 
 def exp_gain():
     data['hero_stats']['experience'] += 1
     for level in data['level_threshold']: 
         if data['hero_stats']['experience'] == data['level_threshold'][level]:
             print('You have reached ' + str(level) + '!!')
 
-print(data['hero_stats']['experience'])
+#Basic enemy 
+class Monster:
+    def __init__(self, MonsterName, MonsterExperience, MonsterLife, MonsterDamage):
+        self.mn = MonsterName
+        self.me = MonsterExperience
+        self.ml = MonsterLife
+        self.md = MonsterDamage
 
+Ogre = Monster("Ogre", 1, 5, 2)
+
+#Basic combat 
+hero_damage = int(data['hero_stats']['strength']) + int(data['hero_stats']['weapon_damage'])
+
+#Main game loop 
 while True: 
+    print("Ogre Spawned")
+    current_monsters_life = int(Ogre.ml)
+    print("Ogre has : " + str(current_monsters_life) + " Life")
+    move = input("Do you attack?")
+    if move == "yes":
+        current_monsters_life -= hero_damage
+    print("Ogre has : " + str(current_monsters_life) + " Life")
+    
+
+
+
     exp_gained = input()
     if exp_gained == "yes":
         exp_gain()
@@ -18,6 +42,3 @@ while True:
 
     
     json.dump(data, open("data.txt", "w"))
-
-   
-    
